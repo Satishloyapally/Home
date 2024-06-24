e302797e34c0bc0954ece6546bcd1a1908fee351
Satish Chandra <42567645+Satishloyapally@users.noreply.github.com>
2024-06-24 18:16:10 -0500

    Create HDD to SSD script.ps1

---
+++ HDD to SSD script.ps1
@@ -0,0 +1,29 @@
+$RG = "gc-kcenter-emea-sit-rg"
+$VMNames = @("GBBE13V0814","GBEX15AS03VCN2")
+$diskType ="StandardSSD_LRS"
+
+foreach($VMName in $VMNames)
+{
+    try{
+            Write-Host "Processing VM = $VMName"
+            Stop-AzVM -ResourceGroupName $RG -Name $VMName -Force -NoWait
+            $vm = Get-AzVM -ResourceGroupName $RG -Name $VMName
+            while($vm.ProvisioningState -ne "Succeeded"){
+                Start-Sleep -Seconds 10
+                $vm = Get-AzVM -ResourceGroupName $RG -Name $VMName
+            }
+            # Get the data Disks
+            $dataDisks = $vm.StorageProfile.DataDisks
+            # Update each Data disk SKU to SSD
+            foreach($dataDisk in $dataDisks){
+                $disk = Get-AzDisk -ResourceGroupName $RG -DiskName $dataDisk.Name
+                $disk.Sku.Name = $diskType
+                $disk | Update-AzDisk
+            }
+            # Start the VM
+            Start-AzVM -ResourceGroupName $RG -Name $VMName
+        }
+        catch{
+            Write-Host "Failed to Update Disks for VM"
+        }
+}
